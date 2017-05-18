5c1870d440a6ce4bac22c77b42dd50a72cf64950
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-05-18 19:27:40 +0200

    pokable

--- restddosaworker.py
+++ restddosaworker.py
@@ -4,6 +4,7 @@ from flask import Flask, url_for, jsonify, send_file, request
 import pylru
 
 import sys
+import time
 import json
 import re
 import os
@@ -26,56 +27,128 @@ context=socket.gethostname()
 
 app = Flask(__name__)
 
+def timestamp():
+    time.strftime("%Y-%m-%dT%H:%M:%S")
 
-def run_dda(target,modules,assume):
-    cmd=["rundda.py",target,"-j","-c"] # it's peculiar but it is a level of isolation
+class Worker(object):
+    task=None
+    all_output=None
 
-    dlog(logging.INFO,"starting "+repr(cmd))
-    
-    for module in modules:
-        cmd+=["-m",module]
+    event_history=[]
 
-    if assume!="":
-        cmd+=["-a",assume]
+    def __init__(self):
+        self.alive_since=timestamp()
 
-    print "$ "+" ".join(cmd)
-    p=subprocess.Popen(cmd,stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
-    all_output=""
-    try:
-        while True:
-            line = p.stdout.readline()
-            if not line:
-                break
-            print '{log:heatool}',line,
-            all_output+=line
+    def format_status(self):
+        return dict(
+                task=self.task,
+                alive_since=self.alive_since,
+            )
 
-        p.wait()
+    def run_dda(self,target,modules,assume,client=None):
+        self.event_history.append(dict(
+            event='requested',
+            target=target,
+            modules=modules,
+            assume=assume,
+            client=client,
+            timestamp=timestamp()
+        ))
 
-        if p.returncode!=0:
-            raise Exception("rundda.py failed with code %i"%p.returncode)
+        if self.task is not None:
+            r=dict(status='busy',task=self.task,output=self.all_output)
+            return r,None,None,None
+        
+        if target == "poke":
+            return self.all_output,self.format_status(),"",""
 
+        if target.startswith("sleep"):
+            self.task="sleeping"
+            self.all_output=""
+            for i in range(int(target.split(":")[1])):
+                self.all_output+="%i\n"%i
+                time.sleep(1)
+            self.task=None
+            return '\nwell slept!\n\n'+self.all_output,{},"",""
+
+
+        cmd=["rundda.py",target,"-j","-c"] # it's peculiar but it is a level of isolation
+
+        dlog(logging.INFO,"starting "+repr(cmd))
+        
+        for module in modules:
+            cmd+=["-m",module]
+
+        if assume!="":
+            cmd+=["-a",assume]
+
+        print "$ "+" ".join(cmd)
+        p=subprocess.Popen(cmd,stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
+
+        self.all_output=""
         try:
-            d=json.load(open("object_data.json"))
-        except:
-            d="unreable"
+            self.task=cmd
 
-        try:
-            h=open("reduced_hashe.txt").read()
-        except:
-            h="unreable"
+            self.event_history.append(dict(
+                event='started',
+                cmd=cmd,
+                timestamp=timestamp()
+            ))
 
-        try:
-            cps=[l.split()[1] for l in open("object_url.txt")]
-        except:
-            cps="unreable"
+            while True:
+                line = p.stdout.readline()
+                if not line:
+                    break
+                print '{log:heatool}',line,
+                self.all_output+=line
 
-        return all_output,d,h,cps
-    except Exception as e:
-        if all_output=="":
-            all_output=p.stdout.read()
+            p.wait()
+            self.task=None
+            
+            self.event_history.append(dict(
+                event='finished',
+                cmd=cmd,
+                timestamp=timestamp()
+            ))
 
-        r=dict(status='ERROR',exception=repr(e),output=all_output)
-        return r,None,None,None
+            if p.returncode!=0:
+                self.event_history.append(dict(
+                    event='rundda failed',
+                    cmd=cmd,
+                    timestamp=timestamp()
+                ))
+                raise Exception("rundda.py failed with code %i"%p.returncode)
+
+            try:
+                d=json.load(open("object_data.json"))
+            except:
+                d="unreable"
+
+            try:
+                h=open("reduced_hashe.txt").read()
+            except:
+                h="unreable"
+
+            try:
+                cps=[l.split()[1] for l in open("object_url.txt")]
+            except:
+                cps="unreable"
+
+            return all_output,d,h,cps
+        except Exception as e:
+            if self.all_output=="":
+                self.all_output=p.stdout.read()
+                
+            self.event_history.append(dict(
+                event='generic failed',
+                cmd=cmd,
+                timestamp=timestamp()
+            ))
+
+            r=dict(status='ERROR',exception=repr(e),output=self.all_output)
+            return r,None,None,None
+
+the_one_worker=Worker()
 
 @app.route('/api/v1.0/<string:target>', methods=['GET'])
 @ddosaauth.requires_auth
@@ -90,7 +163,7 @@ def ddosaworker(target):
         assume=request.args['assume']
 
 
-    result,data,hashe,cached_path=run_dda(target,modules,assume)
+    result,data,hashe,cached_path=the_one_worker.run_dda(target,modules,assume)
 
     r={'modules':modules,'assume':assume,'result':result,'data':data,'hashe':hashe,'cached_path':cached_path}
 
@@ -113,4 +186,4 @@ if __name__ == '__main__':
     dlog(logging.INFO,"starting integral-ddosa-worker")
 
     ##
-    app.run(debug=False,port=port,host=host)
+    app.run(debug=False,port=port,host=host,threaded=True)
