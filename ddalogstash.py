d71d4ac34a07b1e93b7f968d54ed9a0df042d520
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-02-15 13:15:35 +0100

    formatter and no handler

--- ddalogstash.py
+++ ddalogstash.py
@@ -1,14 +1,22 @@
-import logging
-import logstash
 import sys
 import os
 
-host = os.environ.get('LOGSTASH_HOST','dockerelk_logstash_1')
+import logging
+from logstash_formatter import LogstashFormatterV1
 
-logger = logging.getLogger('python-logstash-logger')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logstash.TCPLogstashHandler(host, 5000, version=1))
-logger.addHandler(logging.StreamHandler())
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = LogstashFormatterV1()
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+#host = os.environ.get('LOGSTASH_HOST','dockerelk_logstash_1')
+
+#logger = logging.getLogger('python-logstash-logger')
+#logger.setLevel(logging.DEBUG)
+#logger.addHandler(logstash.TCPLogstashHandler(host, 5000, version=1))
+#logger.addHandler(logging.StreamHandler())
 
 #logger.error('python-logstash: test logstash error message.')
 #logger.info('python-logstash: test logstash info message.')
