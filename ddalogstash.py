dfedb8580105b11aaf2c8d2ebae6c4ed054301c7
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2018-10-14 10:29:37 +0200

    cleanup logstash

--- ddalogstash.py
+++ ddalogstash.py
@@ -11,18 +11,6 @@ formatter = LogstashFormatterV1()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-#host = os.environ.get('LOGSTASH_HOST','dockerelk_logstash_1')
-
-#logger = logging.getLogger('python-logstash-logger')
-#logger.setLevel(logging.DEBUG)
-#logger.addHandler(logstash.TCPLogstashHandler(host, 5000, version=1))
-#logger.addHandler(logging.StreamHandler())
-
-#logger.error('python-logstash: test logstash error message.')
-#logger.info('python-logstash: test logstash info message.')
-#logger.warning('python-logstash: test logstash warning message.')
-
-# add extra field to logstash message
 extra = {
     'python version:':repr(sys.version_info),
 }
